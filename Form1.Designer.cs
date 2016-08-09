ffa747c46c2444edab1182aea2f4c244eb401a51
enrico2371@gmail.com <enrico2371@gmail.com>
2016-08-09 14:22:26 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblCanvas = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(96, 34);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(209, 20);
+            this.txtSource.TabIndex = 2;
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(26, 91);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(60, 13);
+            this.lblDestination.TabIndex = 6;
+            this.lblDestination.Text = "Destination";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(96, 83);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.ReadOnly = true;
+            this.txtDestination.Size = new System.Drawing.Size(209, 20);
+            this.txtDestination.TabIndex = 5;
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(331, 34);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 7;
+            this.btnSource.Text = "Select";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(331, 81);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnDestination.TabIndex = 8;
+            this.btnDestination.Text = "Select";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(26, 34);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(41, 13);
+            this.lblSource.TabIndex = 9;
+            this.lblSource.Text = "Source";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(331, 163);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 11;
+            this.btnConvert.Text = "Resize Images";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(29, 250);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(461, 169);
+            this.txtResult.TabIndex = 12;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(99, 163);
+            this.txtWidth.MaxLength = 4;
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 13;
+            this.txtWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWidth_KeyPress);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(205, 163);
+            this.txtHeight.MaxLength = 4;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 14;
+            this.txtHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHeight_KeyPress);
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(96, 147);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 15;
+            this.lblWidth.Text = "Width";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(202, 147);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 16;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblCanvas
+            // 
+            this.lblCanvas.AutoSize = true;
+            this.lblCanvas.Location = new System.Drawing.Point(32, 163);
+            this.lblCanvas.Name = "lblCanvas";
+            this.lblCanvas.Size = new System.Drawing.Size(43, 13);
+            this.lblCanvas.TabIndex = 17;
+            this.lblCanvas.Text = "Canvas";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(32, 234);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 18;
+            this.lblResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 447);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblCanvas);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Name = "Form1";
+            this.Text = "Image Resizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblCanvas;
+        private System.Windows.Forms.Label lblResult;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fld1 = new FolderBrowserDialog();
+            fld1.RootFolder = Environment.SpecialFolder.Desktop;
+            if (fld1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+               
+            txtSource.Text = fld1.SelectedPath;
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fld2 = new FolderBrowserDialog();
+            fld2.RootFolder = Environment.SpecialFolder.Desktop;
+            
+            if (fld2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtDestination.Text = fld2.SelectedPath;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string source = "";
+            string destination = "";
+            int canvasWidth = 0;
+            int canvasHeight = 0;
+
+            if (string.IsNullOrEmpty(txtSource.Text))
+            {
+                MessageBox.Show("Source is empty.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtDestination.Text))
+            {
+                MessageBox.Show("Destination is empty.");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtWidth.Text))
+            {
+                MessageBox.Show("Canvas width is empty.");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtHeight.Text))
+            {
+                MessageBox.Show("Canvas height is empty.");
+                return;
+            }
+
+            source = txtSource.Text;
+            destination = txtDestination.Text;
+            canvasWidth = Convert.ToInt32(txtWidth.Text);
+            canvasHeight = Convert.ToInt32(txtHeight.Text);
+
+            var result = imageResizer.SaveResizeImage(source, destination, canvasWidth, canvasHeight);
+
+            foreach (var item in result) {
+                txtResult.Text += item.ToString() + System.Environment.NewLine ;
+            }
+
+        }
+
+        private void txtWidth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtHeight_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+       
+    }
+
+    public static class imageResizer
+    {
+        
+        public static List<string> SaveResizeImage(string source = "", string destination = "", int canvasWidth = 0, int canvasHeight = 0)
+        {
+            
+            List<string> files = new List<string>();
+            DirectoryInfo dirImages = new DirectoryInfo(source);
+            ArrayList arrImages = new ArrayList();
+            arrImages.AddRange(dirImages.GetFiles("*.gif"));
+            arrImages.AddRange(dirImages.GetFiles("*.jpg"));
+            arrImages.AddRange(dirImages.GetFiles("*.png"));
+            foreach (FileInfo file in arrImages) {
+               
+                var result = resizeImagePreserveRatio(file.FullName, file.Name, destination, canvasWidth, canvasHeight);
+                
+                files.Add(result);
+            }
+
+            return files;
+        }
+
+        private static string resizeImagePreserveRatio(string fullName = "", string fileName = "", 
+            string destination = "", int canvasWidth = 0, int canvasHeight = 0)
+        {
+            var image = Image.FromFile(fullName);
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+            string fPath = destination + "\\" + fileName;
+            System.Drawing.Imaging.ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+            System.Drawing.Imaging.ImageCodecInfo pngEncoder = GetEncoder(ImageFormat.Png);
+            System.Drawing.Imaging.ImageCodecInfo gifEncoder = GetEncoder(ImageFormat.Gif);
+
+            /* ------------------ begin CALCULATE NEW SIZE, RATIO --------------- */
+
+            // Calculate the ratio or percentage of increase
+            double ratioX = (double)canvasWidth / (double)originalWidth;
+            double ratioY = (double)canvasHeight / (double)originalHeight;
+            // use whichever multiplier is smaller
+            double ratio = ratioX < ratioY ? ratioX : ratioY;
+
+            // Set the new height and width
+            int newHeight = Convert.ToInt32(originalHeight * ratio);
+            int newWidth = Convert.ToInt32(originalWidth * ratio);
+
+            // Calculate the X,Y position of the upper-left corner 
+            int posX = Convert.ToInt32((canvasWidth - (originalWidth * ratio)) / 2);
+            int posY = Convert.ToInt32((canvasHeight - (originalHeight * ratio)) / 2);
+            /* ------------- end CALCULATE NEW SIZE, RATIO ---------------- */
+
+            /* ------------------ begin Resize and repaint  --------------- */
+            System.Drawing.Bitmap canvas = new Bitmap(canvasWidth, canvasHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            System.Drawing.Graphics graphic = System.Drawing.Graphics.FromImage(canvas);
+            graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphic.SmoothingMode = SmoothingMode.HighQuality;
+            graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            graphic.CompositingQuality = CompositingQuality.HighQuality;
+
+            EncoderParameters encoderParameters;
+            encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+            
+            
+
+            if (image.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Png))
+            {
+                graphic.Clear(Color.Transparent);
+                graphic.DrawImage(image, posX, posY, newWidth, newHeight);
+                canvas.MakeTransparent();
+                canvas.Save(fPath, pngEncoder, encoderParameters);
+            }
+            else 
+            {
+                graphic.Clear(Color.White);
+                graphic.DrawImage(image, posX, posY, newWidth, newHeight);
+                canvas.Save(fPath, jpgEncoder, encoderParameters);
+            }
+            
+
+            /* ------------- end REsize and repaint ---------------- */
+
+            image.Dispose();
+            graphic.Dispose();
+            canvas.Dispose();
+
+            return "Image Saved : " + fPath;
+
+           
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+    }
+
+
+}
